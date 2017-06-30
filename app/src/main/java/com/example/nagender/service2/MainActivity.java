8683ce78775a5aee6092f0dd7229780e33a1bad2
nagpamu <nagpa240@gmail.com>
2017-06-29 22:44:35 -0500

    added new components

--- app/src/main/java/com/example/nagender/service2/MainActivity.java
+++ app/src/main/java/com/example/nagender/service2/MainActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//nagender
 public class MainActivity extends Activity {
 
     @Override
